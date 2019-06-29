0829846968e3b972862832a31f9e42ab48209d2f
Laura <lmcsay@gmail.com>
2019-06-29 14:01:06 -0500

    Form Validation

--- public/js/form_validation.js
+++ public/js/form_validation.js
@@ -1,43 +1,40 @@
-$(document).ready(function () {
-    $(".name-not-valid").hide();
-    console.log("hit");
+// $(document).ready(function () {
+//     $(".name-not-valid").hide();
 
-    $(document).on("click", ".search_name", isValidName);
-    function isValidName(name) {
-        event.preventDefault();
-        var name = $('.search_by_name').val();
-        if (name == "" || name == null) {
-            $(".name-not-valid").show();
-            return false;
-        } else {
-            $(".name-not-valid").hide();
-            return true
-        }
-
-    }
-
-    $(document).on("click", ".search_other", isFieldEntered);
-    function isFieldEntered(name) {
-        event.preventDefault();
-        var radios = document.getElementsByName("rating");
-        var ratingSelected = false;
     
-        var i = 0;
-        while (!ratingSelected && i < radios.length) {
-            if (radios[i].checked) {
-                ratingSelected = true;
-                i++;    
-            }    
-        }
+//     function isValidName(name) {
+//         event.preventDefault();
+//         var name = $('.search_by_name').val();
+//         if (name == "" || name == null) {
+//             $(".name-not-valid").show();
+//             return false;
+//         } else {
+//             $(".name-not-valid").hide();
+//             return true
+//         }
+
+//     $(document).on("click", ".search_other", isFieldEntered);
+//     function isFieldEntered(name) {
+//         event.preventDefault();
+//         var radios = document.getElementsByName("rating");
+//         var ratingSelected = false;
     
-        if (!ratingSelected) {
+//         var i = 0;
+//         while (!ratingSelected && i < radios.length) {
+//             if (radios[i].checked) {
+//                 ratingSelected = true;
+//                 i++;    
+//             }    
+//         }
+    
+//         if (!ratingSelected) {
 
-        }
-        return formValid;
+//         }
+//         return formValid;
 
-    }
+//     }
 
-    // add this to html
-    // <div class="name-not-valid">Please enter valid recipe name</div>
+//     // add this to html
+//     // <div class="name-not-valid">Please enter valid recipe name</div>
 
-})
+// })
