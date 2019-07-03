9070b541f57e186394adc18a424991e4655a4507
Zeke <ezekielbadowski@gmail.com>
2019-07-02 21:34:27 -0500

    H Fix

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 var mysql = require("mysql");
 var config = require("./config");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-var connection = mysql.createConnection({
-  host: "localhost", 
-  port: 8889, 
-  user: "root", 
-  password: "root", 
-  database: "celz" 
-});
 
-connection.connect(function(err) {
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "celz"
+  });
+}
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
